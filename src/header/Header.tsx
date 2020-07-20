3a5cbad028bd22bfc7c494159694c8fe82270f26
mori-san <htt.mapp@gmail.com>
2020-07-20 16:43:00 +0800

    Add header

---
+++ src/header/Header.tsx
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { observer } from "mobx-react";
+import { withRouter, RouteComponentProps } from "react-router";
+import "./Header.css";
+import Nav from "react-bootstrap/Nav";
+
+//=======================================================
+// The header.
+//=======================================================
+@observer
+class Header extends Component<RouteComponentProps> {
+  render() {
+    return (
+      <div className="Header">
+        <div className="Header-logo">morisan</div>
+
+        <Nav className="justify-content-center" activeKey="/homepage">
+          <Nav.Item>
+            <Nav.Link href="/homepage">Home</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link href="/aboutme">About Me</Nav.Link>
+          </Nav.Item>
+        </Nav>
+      </div>
+    );
+  }
+}
+
+export default withRouter(Header);
