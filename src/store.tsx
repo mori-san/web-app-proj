2983cfeeb3f722698a83b47e303388fb5ded921e
mori-san <htt.mapp@gmail.com>
2020-07-21 16:02:25 +0800

    Add state for sample page

--- src/store.tsx
+++ src/store.tsx
@@ -1,13 +1,16 @@
 import HomepageState from "./content/homepage/HomepageState";
 import AboutmeState from "./content/aboutme/AboutmeState";
+import SampleState from "./content/sample/SampleState";
 
 //=======================================================
 // States for all the pages.
 //=======================================================
 const homepageState = new HomepageState();
 const aboutmeState = new AboutmeState();
+const sampleState = new SampleState();
 
 export default {
   homepageState: homepageState,
   aboutmeState: aboutmeState,
+  sampleState: sampleState,
 };
