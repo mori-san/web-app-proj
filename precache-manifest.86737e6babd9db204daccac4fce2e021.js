d46869a462c45f8139bfda6d2ff0f45d0ea3da6b
mori-san <htt.mapp@gmail.com>
2020-07-23 17:09:45 +0800

    Updates

---
+++ precache-manifest.86737e6babd9db204daccac4fce2e021.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2edb13ceea87be9218d3fa4d8d2c49f6",
+    "url": "/web-app-proj/index.html"
+  },
+  {
+    "revision": "bf5d1ff2d222b1f54dca",
+    "url": "/web-app-proj/static/css/main.0a8acedc.chunk.css"
+  },
+  {
+    "revision": "4cc1eede28061ed1025a",
+    "url": "/web-app-proj/static/js/2.89bad063.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/web-app-proj/static/js/2.89bad063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5d1ff2d222b1f54dca",
+    "url": "/web-app-proj/static/js/main.984d841d.chunk.js"
+  },
+  {
+    "revision": "6a68894c11c83c1be7a7",
+    "url": "/web-app-proj/static/js/runtime-main.9b139e24.js"
+  },
+  {
+    "revision": "c4b75db639bba0f8c08144ca3a52bbaa",
+    "url": "/web-app-proj/static/media/heart.c4b75db6.svg"
+  },
+  {
+    "revision": "e8c5d072fee6cccefb41956105603534",
+    "url": "/web-app-proj/static/media/tick.e8c5d072.svg"
+  },
+  {
+    "revision": "3a1408c99a886aab6c926f7c05f868a6",
+    "url": "/web-app-proj/static/media/timeline.3a1408c9.svg"
+  }
+]);
