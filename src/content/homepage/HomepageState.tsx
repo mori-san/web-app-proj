254fe67ac32c993e718681feca89902f9ec8308d
mori-san <htt.mapp@gmail.com>
2020-07-27 14:18:03 +0800

    Remove constructor

--- src/content/homepage/HomepageState.tsx
+++ src/content/homepage/HomepageState.tsx
@@ -4,8 +4,6 @@ import { action } from "mobx";
 // The state of the homepage
 //=======================================================
 export default class HomepageState {
-  constructor() {}
-
   @action
   goToGitHubRepository = () => {
     window.open("https://github.com/mori-san/web-app-proj");
