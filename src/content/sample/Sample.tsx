1ae6b6d28cbdb8c330e755e45053df1210f49be1
mori-san <htt.mapp@gmail.com>
2020-07-27 14:26:57 +0800

    Add weather forecast api to sample page

--- src/content/sample/Sample.tsx
+++ src/content/sample/Sample.tsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { observer } from "mobx-react";
 import "./Sample.css";
 import SampleState from "./SampleState";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 //=======================================================
 // Props that sample page is receiving.
@@ -15,9 +17,67 @@ type SampleProps = {
 //=======================================================
 @observer
 export default class Sample extends Component<SampleProps> {
+  componentDidMount() {
+    this.props.state.getWeatherForecast();
+  }
+
+  scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
   render() {
     return (
       <div className="Sample">
+        <div className="Sample-box">
+          <div className="Sample-title">RESTful API Integration</div>
+
+          <Card className="text-center">
+            <Card.Header>24-hour Weather Forecast</Card.Header>
+            <Card.Body>
+              <Card.Title>Singapore</Card.Title>
+              <Card.Text>
+                <b>{this.props.state.weatherData.forecast}</b>
+              </Card.Text>
+              <Card.Text>
+                Temperature:{" "}
+                <b>
+                  {this.props.state.weatherData.temperature.low} -{" "}
+                  {this.props.state.weatherData.temperature.high}°C
+                </b>
+              </Card.Text>
+              <Card.Text>
+                Wind:{" "}
+                <b>
+                  {this.props.state.weatherData.wind.direction}{" "}
+                  {this.props.state.weatherData.wind.speed.low} -{" "}
+                  {this.props.state.weatherData.wind.speed.high} km/h
+                </b>
+              </Card.Text>
+              <Card.Text>
+                Humidity:{" "}
+                <b>
+                  {this.props.state.weatherData.relativeHumidity.low} -{" "}
+                  {this.props.state.weatherData.relativeHumidity.high} %
+                </b>
+              </Card.Text>
+              <Card.Text>
+                <b>GET </b>
+                https://api.data.gov.sg/v1/environment/24-hour-weather-forecast
+              </Card.Text>
+              <Card.Text>
+                Retrieve the latest 24 hour weather forecast
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Button
+            className="backtotop-button"
+            variant="outline-secondary"
+            onClick={this.scrollToTop}
+          >
+            ↑ Back to Top
+          </Button>
+        </div>
       </div>
     );
   }
--- src/content/sample/SampleState.tsx
+++ src/content/sample/SampleState.tsx
@@ -1,6 +1,43 @@
+import { observable, action } from "mobx";
+import { WeatherForecast, WEATHERFORECAST_DEFAULT } from "../../types";
+
+const axios = require("axios");
+
 //=======================================================
 // The state of the sample page
 //=======================================================
 export default class SampleState {
-  constructor() {}
+  @observable weatherData: WeatherForecast;
+
+  constructor() {
+    this.weatherData = WEATHERFORECAST_DEFAULT;
+  }
+
+  //=======================================================
+  // API Calls.
+  //=======================================================
+  @action
+  getWeatherForecast = async () => {
+    try {
+      const response: any = await axios.get(
+        "https://api.data.gov.sg/v1/environment/24-hour-weather-forecast"
+      );
+      console.log("Weather Forecast", response);
+      response.data.items.forEach((data: any) => {
+        data.periods.forEach((period: any, index: number) => {
+          this.weatherData.periods.splice(index, 0, period);
+        });
+        this.weatherData.forecast = data.general.forecast;
+        this.weatherData.relativeHumidity = data.general.relative_humidity;
+        this.weatherData.temperature = data.general.temperature;
+        this.weatherData.wind.direction = data.general.wind.direction;
+        this.weatherData.wind.speed = data.general.wind.speed;
+        this.weatherData.timestamp = data.timestamp;
+        this.weatherData.updateTimestamp = data.update_timestamp;
+        this.weatherData.validPeriod = data.valid_period;
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
 }
