02f705e27e79a1ab94cd4886a7ad01233be2af8d
mori-san <htt.mapp@gmail.com>
2020-07-27 14:33:42 +0800

    Add currency conversion api to sample page

--- src/content/sample/Sample.tsx
+++ src/content/sample/Sample.tsx
@@ -3,6 +3,8 @@ import { observer } from "mobx-react";
 import "./Sample.css";
 import SampleState from "./SampleState";
 import Card from "react-bootstrap/Card";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
 
 //=======================================================
@@ -18,6 +20,8 @@ type SampleProps = {
 @observer
 export default class Sample extends Component<SampleProps> {
   componentDidMount() {
+    this.props.state.refreshData();
+    this.props.state.getCurrencies();
     this.props.state.getWeatherForecast();
   }
 
@@ -30,6 +34,100 @@ export default class Sample extends Component<SampleProps> {
       <div className="Sample">
         <div className="Sample-box">
           <div className="Sample-title">RESTful API Integration</div>
+
+          <Card className="text-center">
+            <Card.Header>Currency Conversion</Card.Header>
+            <Card.Body>
+              <Form>
+                <Form.Row>
+                  <Form.Group as={Col}>
+                    <Form.Label>Amount</Form.Label>
+                    <Form.Control
+                      as="input"
+                      value={this.props.state.amount}
+                      onChange={({ target }) =>
+                        this.props.state.setAmount(parseFloat(target.value))
+                      }
+                      type="number"
+                      min="0"
+                      isValid={this.props.state.amount !== 0}
+                      isInvalid={this.props.state.amount === 0}
+                    ></Form.Control>
+                  </Form.Group>
+                  <Form.Group as={Col}>
+                    <Form.Label>From</Form.Label>
+                    <Form.Control
+                      as="select"
+                      custom
+                      value={this.props.state.fromCurrencyCode}
+                      onChange={({ target }) =>
+                        this.props.state.selectFromCurrencyCode(
+                          String(target.value)
+                        )
+                      }
+                      isValid={
+                        this.props.state.fromCurrencyCode !==
+                        this.props.state.toCurrencyCode
+                      }
+                      isInvalid={this.props.state.currencyCodeError}
+                    >
+                      {this.props.state.currencyList.map((option, index) => {
+                        return (
+                          <option key={index} value={option.code}>
+                            {option.currency}
+                          </option>
+                        );
+                      })}
+                    </Form.Control>
+                  </Form.Group>
+                  <Form.Group as={Col}>
+                    <Form.Label>To</Form.Label>
+                    <Form.Control
+                      as="select"
+                      custom
+                      value={this.props.state.toCurrencyCode}
+                      onChange={({ target }) =>
+                        this.props.state.selectToCurrencyCode(
+                          String(target.value)
+                        )
+                      }
+                      isValid={
+                        this.props.state.toCurrencyCode !==
+                        this.props.state.fromCurrencyCode
+                      }
+                      isInvalid={this.props.state.currencyCodeError}
+                    >
+                      {this.props.state.currencyList.map((option, index) => {
+                        return (
+                          <option key={index} value={option.code}>
+                            {option.currency}
+                          </option>
+                        );
+                      })}
+                    </Form.Control>
+                  </Form.Group>
+                </Form.Row>
+              </Form>
+              {this.props.state.conversionResult.toAmount > 0 ? (
+                <React.Fragment>
+                  <Card.Title>
+                    {this.props.state.conversionResult.fromAmount}{" "}
+                    {this.props.state.conversionResult.fromCurrency} =
+                  </Card.Title>
+                  <Card.Text>
+                    {this.props.state.conversionResult.toAmount}{" "}
+                    {this.props.state.conversionResult.toCurrency}
+                  </Card.Text>
+                </React.Fragment>
+              ) : null}
+              <Button
+                variant="primary"
+                onClick={this.props.state.getExchangeRate}
+              >
+                Convert
+              </Button>
+            </Card.Body>
+          </Card>
 
           <Card className="text-center">
             <Card.Header>24-hour Weather Forecast</Card.Header>
--- src/content/sample/SampleState.tsx
+++ src/content/sample/SampleState.tsx
@@ -1,5 +1,10 @@
-import { observable, action } from "mobx";
-import { WeatherForecast, WEATHERFORECAST_DEFAULT } from "../../types";
+import { observable, action, computed } from "mobx";
+import {
+  CurrencyConversion,
+  CURRENCYCONVERSION_DEFAULT,
+  WeatherForecast,
+  WEATHERFORECAST_DEFAULT,
+} from "../../types";
 
 const axios = require("axios");
 
@@ -7,15 +12,114 @@ const axios = require("axios");
 // The state of the sample page
 //=======================================================
 export default class SampleState {
+  @observable currencyList: any[];
+  @observable amount: number;
+  @observable fromCurrencyCode: string;
+  @observable toCurrencyCode: string;
+  @observable conversionResult: CurrencyConversion;
+  @observable showCurrencyCodeError: boolean;
+
   @observable weatherData: WeatherForecast;
 
   constructor() {
+    this.currencyList = [];
+    this.amount = 1;
+    this.fromCurrencyCode = "AUD";
+    this.toCurrencyCode = "AUD";
+    this.conversionResult = CURRENCYCONVERSION_DEFAULT;
+    this.showCurrencyCodeError = false;
+
     this.weatherData = WEATHERFORECAST_DEFAULT;
+  }
+
+  @action
+  refreshData = () => {
+    this.currencyList = [];
+    this.amount = 1;
+    this.fromCurrencyCode = "AUD";
+    this.toCurrencyCode = "AUD";
+    this.conversionResult = CURRENCYCONVERSION_DEFAULT;
+    this.showCurrencyCodeError = false;
+  };
+
+  @action
+  setAmount = (amount: number) => {
+    if (amount) {
+      this.amount = amount;
+    } else {
+      this.amount = 0;
+    }
+  };
+
+  @action
+  selectFromCurrencyCode = (code: string) => {
+    this.fromCurrencyCode = code;
+  };
+
+  @action
+  selectToCurrencyCode = (code: string) => {
+    this.toCurrencyCode = code;
+  };
+
+  @computed
+  get currencyCodeError(): boolean {
+    return (
+      this.showCurrencyCodeError &&
+      this.fromCurrencyCode === this.toCurrencyCode
+    );
   }
 
   //=======================================================
   // API Calls.
   //=======================================================
+  @action
+  getCurrencies = async () => {
+    try {
+      const response: any = await axios.get(
+        "https://api.frankfurter.app/currencies"
+      );
+      console.log("Currencies", response);
+      Object.entries(response.data).forEach((data: any, index: number) => {
+        this.currencyList.splice(index, 0, {
+          code: data[0],
+          currency: data[1],
+        });
+      });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  @action
+  getExchangeRate = async () => {
+    if (this.fromCurrencyCode === this.toCurrencyCode) {
+      this.showCurrencyCodeError = true;
+      return;
+    }
+    this.showCurrencyCodeError = false;
+    try {
+      axios
+        .get("https://api.frankfurter.app/latest", {
+          params: {
+            amount: this.amount,
+            from: this.fromCurrencyCode,
+            to: this.toCurrencyCode,
+          },
+        })
+        .then((response: any) => {
+          console.log("Exchange Rate", response);
+          this.conversionResult.fromAmount = response.data.amount;
+          this.conversionResult.fromCurrency = response.data.base;
+          Object.entries(response.data.rates).forEach((rates: any) => {
+            this.conversionResult.toCurrency = rates[0];
+            this.conversionResult.toAmount = rates[1];
+          });
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   @action
   getWeatherForecast = async () => {
     try {
